81424be20d2c840ca3058b89959d740d37b01cae
CasperStaahl <cstahl20@student.aau.dk>
2021-11-02 20:49:12 +0100

    Create Username.cs

---
+++ Username.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace User
+{
+    public class Username
+    {
+        private static readonly Regex _validator = new Regex(@"^([a-z0-9_])+$");
+
+        public string String
+        {
+            get { return _string; }
+            set
+            {
+                if (_validator.IsMatch(value))
+                {
+                    _string = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        private string _string = "nil";
+    }
+}
