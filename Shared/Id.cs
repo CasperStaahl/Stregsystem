152bee2d4eec89ecd94f05fe35dbd9ca6ed655b4
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-05 14:58:12 +0100

    Move Id.cs to /shared

--- Shared/Id.cs
+++ Shared/Id.cs
@@ -1,10 +1,10 @@
-namespace Shared
+namespace Stregsystem.Shared
 {
     public class Id
     {
         private static int _nextNumber = 1;
         public int Number { get; } = 0;
-        
+
         public Id()
         {
             Number = _nextNumber++;
