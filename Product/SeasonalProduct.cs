d04ef899c3e4a53809b7b6cd2402cb12b567057c
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-05 14:59:43 +0100

    Create SeasonalProduct.cs

---
+++ Product/SeasonalProduct.cs
@@ -0,0 +1,51 @@
+using System;
+using Stregsystem.Shared;
+
+namespace Stregsystem.Product
+{
+    class SeasonalProduct : Product
+    {
+        public DateTime SeasonStartDate
+        {
+            get => _seasonStartDate;
+            private set
+            {
+                if (value < _seasonEndDate)
+                {
+                    _seasonStartDate = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        public DateTime SeasonEndDate
+        {
+            get => SeasonEndDate;
+            private set
+            {
+                if (_seasonStartDate < value)
+                {
+                    _seasonEndDate = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        private DateTime _seasonStartDate = new DateTime();
+        private DateTime _seasonEndDate = new DateTime();
+
+        public SeasonalProduct(Name name, DDK price, bool active, bool canBeBoughtOnCredit,
+        DateTime seasonStartDate, DateTime seasonEndDate)
+        : base(name, price, active, canBeBoughtOnCredit)
+        {
+            _seasonStartDate = seasonStartDate;
+            _seasonEndDate = seasonEndDate;
+        }
+    }
+}
