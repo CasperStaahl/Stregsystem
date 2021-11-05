9ac66d991a244cf00ad5811ec86b0ce71602a75a
Casper Ståhl <cstahl20@student.aau.dk>
2021-11-05 14:59:46 +0100

    Create InsertCashTransaction.cs

---
+++ Transactions/InsertCashTransaction.cs
@@ -0,0 +1,22 @@
+﻿using Stregsystem.Shared;
+using Stregsystem.Users;
+using System;
+
+namespace Stregsystem.Transactions
+{
+    internal class InsertCashTransaction : Transaction
+    {
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"Insert: {base.ToString()}";
+        }
+
+        public InsertCashTransaction(User user, DateTime date, DDK amount) : base(user, date, amount) { }
+
+    }
+}
