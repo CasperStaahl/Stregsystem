269b204141c4ff945b424f42d36f6bd6c3b4f3a8
CasperStaahl <cstahl20@student.aau.dk>
2021-11-06 17:24:51 +0100

    Create ProductIsNotActiveException.cs

---
+++ Transactions/ProductIsNotActiveException.cs
@@ -0,0 +1,13 @@
+﻿namespace Stregsystem.Transactions
+{
+    [System.Serializable]
+    public class ProductIsNotActiveException : System.Exception
+    {
+        public ProductIsNotActiveException() { }
+        public ProductIsNotActiveException(string message) : base(message) { }
+        public ProductIsNotActiveException(string message, System.Exception inner) : base(message, inner) { }
+        protected ProductIsNotActiveException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
