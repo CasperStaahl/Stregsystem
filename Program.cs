3a432d2a51509c1abd85a5da88a43f144cdbc3be
CasperStaahl <cstahl20@student.aau.dk>
2021-11-06 16:20:07 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Stregsystem
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
