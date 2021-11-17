e8c09ce97eb058317f99ea596a6645a6b58b426c
CasperStaahl <cstahl20@student.aau.dk>
2021-11-17 22:39:14 +0100

    Update Stregsystem.cs

--- Stregsystem/Stregsystem.cs
+++ Stregsystem/Stregsystem.cs
@@ -4,14 +4,14 @@ using Stregsystem.Transactions;
 using Stregsystem.Shared;
 using Stregsystem.Products;
 using Stregsystem.Users;
+using System.Linq;
+using System.IO;
 
 namespace Stregsystem
 {
     internal class Stregsystem
     {
-        // private IEnumerable<Product> ActiveProducts { get; set; } 
-
-        public IEnumerable<Product> ActiveProducts { get; } 
+        public IEnumerable<Product> ActiveProducts { get => _products.FindAll(x => x.IsActive); }
 
         private List<Product> _products = new List<Product>();
 
@@ -41,13 +41,9 @@ namespace Stregsystem
         {
             List<Product> found = _products.FindAll(x => x.Id.Number == idNumber);
             if (0 < found.Count)
-            {
                 return found[0];
-            }
             else
-            {
                 throw new ProductDoesNotExistException($"product with id {idNumber} does not exist");
-            }
         }
 
         private IEnumerable<User> GetUsers(Predicate<User> predicate)
@@ -59,18 +55,31 @@ namespace Stregsystem
         {
             List<User> found = _users.FindAll(x => username.ToString() == x.Username.ToString());
             if (0 < found.Count)
-            {
                 return found[0];
-            }
             else
-            {
                 throw new UserDoesNotExistException($"user with username {username.ToString()} does not exist");
-            }
         }
 
         private IEnumerable<Transaction> GetTransactions(User user, int count)
         {
-            return _transactions.FindAll(x => x.User == user);
+            return _transactions.FindAll(x => x.User == user).Take(count);
+        }
+
+        // This constructur will instantiate a Stregsystem given two file addresses. 
+        // If either Address is invalid it will throw an error.
+        public Stregsystem(string productFileAddress, string userFileAddress)
+        {
+            // For each line, except the first, in the productFile add a product object to _products
+            foreach (string line in File.ReadLines(productFileAddress).Skip(1))
+            {
+                string[] subs = line.Split(';');
+
+                // Product product = new Product();
+                
+            }
+
+            // For each line in the userFile add a user object to _users
+
         }
     }
 }
