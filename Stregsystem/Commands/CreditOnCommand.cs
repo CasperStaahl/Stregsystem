56386803b7a46c4927a31f9a829edc0a53051aad
CasperStaahl <cstahl20@student.aau.dk>
2021-12-05 18:28:15 +0100

    Update CreditOnCommand.cs

--- Stregsystem/Commands/CreditOnCommand.cs
+++ Stregsystem/Commands/CreditOnCommand.cs
@@ -1,6 +1,24 @@
-namespace Stregsystem
+using Stregsystem.Products;
+
+namespace Stregsystem.Commands
 {
     internal class CreditOnCommand : ICommand
     {
+        private IStregsystem _stregsystem;
+        private IStregsystemUI _ui;
+        private int _productId;
+
+        public CreditOnCommand(IStregsystem stregsystem, IStregsystemUI ui, int productId)
+        {
+            _stregsystem = stregsystem;
+            _ui = ui;
+            _productId = productId;
+        }
+
+        public void Execute()
+        {
+            Product product = _stregsystem.GetProductById(_productId);
+            product.CanBeBoughtOnCredit = true;
+        }
     }
 }
