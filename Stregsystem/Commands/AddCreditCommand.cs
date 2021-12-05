7586396529703fe3fe4df477a454ad9084f07b00
CasperStaahl <cstahl20@student.aau.dk>
2021-12-05 18:28:04 +0100

    Update AddCreditCommand.cs

--- Stregsystem/Commands/AddCreditCommand.cs
+++ Stregsystem/Commands/AddCreditCommand.cs
@@ -1,6 +1,27 @@
-namespace Stregsystem
+using Stregsystem.Shared;
+using Stregsystem.Users;
+
+namespace Stregsystem.Commands
 {
     internal class AddCreditCommand : ICommand
     {
+        private IStregsystem _stregsystem;
+        private IStregsystemUI _ui;
+        private Username _username;
+        private Ddk _credit;
+
+        public AddCreditCommand(IStregsystem stregsystem, IStregsystemUI ui, Username username, Ddk credit)
+        {
+            _stregsystem = stregsystem;
+            _ui = ui;
+            _username = username;
+            _credit = credit;
+        }
+
+        public void Execute()
+        {
+            User user = _stregsystem.GetUserByUsername(_username);
+            _stregsystem.AddCreditToAccount(user, _credit);
+        }
     }
 }
