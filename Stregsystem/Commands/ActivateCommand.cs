1be11c8560cf250d11ad5c55a0fef53215705336
CasperStaahl <cstahl20@student.aau.dk>
2021-12-05 18:28:00 +0100

    Update ActivateCommand.cs

--- Stregsystem/Commands/ActivateCommand.cs
+++ Stregsystem/Commands/ActivateCommand.cs
@@ -1,6 +1,24 @@
-namespace Stregsystem
+using Stregsystem.Products;
+
+namespace Stregsystem.Commands
 {
     internal class ActivateCommand : ICommand
     {
+        private IStregsystem _stregsystem;
+        private IStregsystemUI _ui;
+        private int _productId;
+
+        public ActivateCommand(IStregsystem stregsystem, IStregsystemUI ui, int productId)
+        {
+            _stregsystem = stregsystem;
+            _ui = ui;
+            _productId = productId;
+        }
+
+        public void Execute()
+        {
+            Product product = _stregsystem.GetProductById(_productId);
+            product.IsActive = true;
+        }
     }
 }
