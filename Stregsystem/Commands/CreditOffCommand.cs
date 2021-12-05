ec1128d014971c2bbba8d52830f6c957346b8d3a
CasperStaahl <cstahl20@student.aau.dk>
2021-12-05 18:28:13 +0100

    Update CreditOffCommand.cs

--- Stregsystem/Commands/CreditOffCommand.cs
+++ Stregsystem/Commands/CreditOffCommand.cs
@@ -1,6 +1,24 @@
-namespace Stregsystem
+using Stregsystem.Products;
+
+namespace Stregsystem.Commands
 {
     internal class CreditOffCommand : ICommand
     {
+        private IStregsystem _stregsystem;
+        private IStregsystemUI _ui;
+        private int _productId;
+
+        public CreditOffCommand(IStregsystem stregsystem, IStregsystemUI ui, int productId)
+        {
+            _stregsystem = stregsystem;
+            _ui = ui;
+            _productId = productId;
+        }
+
+        public void Execute()
+        {
+            Product product = _stregsystem.GetProductById(_productId);
+            product.CanBeBoughtOnCredit = false; 
+        }
     }
 }
