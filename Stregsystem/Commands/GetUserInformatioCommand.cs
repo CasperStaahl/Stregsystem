4e57010ccdfd0aaaafb65622e92e1fdbf5365a85
CasperStaahl <cstahl20@student.aau.dk>
2021-12-05 18:28:19 +0100

    Update GetUserInformatioCommand.cs

--- Stregsystem/Commands/GetUserInformatioCommand.cs
+++ Stregsystem/Commands/GetUserInformatioCommand.cs
@@ -1,11 +1,24 @@
+using Stregsystem.Users;
+
 namespace Stregsystem
 {
     internal class GetUserInformatioCommand : ICommand
     {
+        private IStregsystem _stregsystem;
+        private IStregsystemUI _ui;
+        private Username _username;
+
+        public GetUserInformatioCommand(IStregsystem stregsystem, IStregsystemUI ui, Username username)
+        {
+            _stregsystem = stregsystem;
+            _ui = ui;
+            _username = username;
+        }
 
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            User user = _stregsystem.GetUserByUsername(_username);
+            _ui.DisplayUserInfo(user);
         }
     }
 }
