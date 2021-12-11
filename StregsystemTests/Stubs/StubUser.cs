4a170a3c8067b9987fbb97b91b2d2d240f9b610d
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 00:42:42 +0100

    Create StubUser.cs

---
+++ StregsystemTests/Stubs/StubUser.cs
@@ -0,0 +1,17 @@
+using System;
+using Stregsystem.Shared;
+using Stregsystem.Users;
+using StregsystemTests.Stubs;
+
+public class StubUser : IUser
+{
+    public IId<IUser> Id => new StubId<IUser>();
+
+    public Username Username { get; set; } = new Username("stub");
+
+    public Ddk Balance { get; set; } = new Ddk(0);
+
+    public event EventHandler BelowBalanceThreshold;
+
+    public int CompareTo(User other) { return 1; }
+}
