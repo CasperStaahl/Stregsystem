d9ec3729469d68d9e6b9bdfd53945a631431a7a9
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 18:40:22 +0100

    Update SeasonalProductTests.cs

--- StregsystemTests/ProductsTests.cs/SeasonalProductTests.cs
+++ StregsystemTests/ProductsTests.cs/SeasonalProductTests.cs
@@ -7,35 +7,21 @@ namespace StregsystemTests.ProductsTests.cs
 {
     public class SeasonalProductTests
     {
-        // [Theory]
-        [InlineData(1, 0, 0, 1, 0, 0)]
-        [InlineData(0, 1, 0, 0, 1, 0)]
-        [InlineData(0, 0, 1, 0, 0, 1)]
-        [InlineData(7, 4, 3, 2, 8, 4)]
-        public void IsActiveBetweenStartDateAndEndDate(
-                                                       int startDateMinutesOffset,
-                                                       int startDateHoursOffset,
-                                                       int startDateDaysOffset,
-                                                       int endDateMinutesOffset,
-                                                       int endDateHoursOffset,
-                                                       int endDateDaysOffset)
-        {
-            // Given
-            SeasonalProduct product
-                = new SeasonalProduct(1,
-                                      new Name("test"),
-                                      new Ddk(100),
-                                      true,
-                                      true,
-                                      DateTime.Now.AddMinutes(-startDateMinutesOffset)
-                                                  .AddHours(-startDateHoursOffset)
-                                                  .AddDays(-startDateDaysOffset),
-                                      DateTime.Now.AddMinutes(endDateMinutesOffset)
-                                                  .AddHours(endDateHoursOffset)
-                                                  .AddDays(endDateDaysOffset));
-            // When
-            // Then
-            Assert.True(product.IsActive);
-        }
+        // [Fact]
+        // public void IsActiveBetweenStartDateAndEndDate()
+        // {
+        //     // Given
+        //     SeasonalProduct product
+        //         = new SeasonalProduct(1,
+        //                               new Name("test"),
+        //                               new Ddk(100),
+        //                               true,
+        //                               true,
+        //                               DateTime.Now.AddMinutes(1),
+        //                               DateTime.Now.AddMinutes(1));
+        //     // When
+        //     // Then
+        //     Assert.True(product.IsActive);
+        // }
     }
 }
