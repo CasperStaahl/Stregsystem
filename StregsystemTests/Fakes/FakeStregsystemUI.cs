838857ca1aa9461cf6575f6e0ca2c4d91aaa6a0b
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 23:32:38 +0100

    Create FakeStregsystemUI.cs

---
+++ StregsystemTests/Fakes/FakeStregsystemUI.cs
@@ -0,0 +1,38 @@
+using System;
+using Stregsystem;
+using Stregsystem.Products;
+using Stregsystem.Transactions;
+using Stregsystem.Users;
+
+namespace StregsystemTests.Fakes
+{
+    public class FakeStregsystemUI : IStregsystemUI
+    {
+        public event EventHandler<string> CommandEntered;
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayGeneralError(string errorString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayUserBuysProduct(BuyTransaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayUserInfo(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
