f1cbe48449790bb4cd394063434ae86e0fab96f1
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 19:56:53 +0100

    Update StubStregsystemUI.cs

--- StregsystemTests/Stubs/StubStregsystemUI.cs
+++ StregsystemTests/Stubs/StubStregsystemUI.cs
@@ -15,27 +15,7 @@ namespace StregsystemTests.Stubs
             throw new NotImplementedException();
         }
 
-        public void DisplayAdminCommandNotFoundMessage(string adminCommand)
-        {
-            throw new NotImplementedException();
-        }
-
         public void DisplayGeneralError(string errorString)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DisplayInsufficientCash(User user, Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DisplayProductNotFound(string product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DisplayTooManyArgumentsError(string command)
         {
             throw new NotImplementedException();
         }
@@ -45,17 +25,7 @@ namespace StregsystemTests.Stubs
             throw new NotImplementedException();
         }
 
-        public void DisplayUserBuysProduct(int count, BuyTransaction transaction)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DisplayUserInfo(User user)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DisplayUserNotFound(string username)
+        public void DisplayUserInfo(IUser user)
         {
             throw new NotImplementedException();
         }
