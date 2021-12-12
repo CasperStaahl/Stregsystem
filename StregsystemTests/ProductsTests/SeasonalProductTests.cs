24293a507052d6e2ed86b7a417a92c1552f300bc
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 23:32:41 +0100

    Create SeasonalProductTests.cs

---
+++ StregsystemTests/ProductsTests/SeasonalProductTests.cs
@@ -0,0 +1,84 @@
+using Stregsystem.Products;
+using StregsystemTests.Fakes;
+using Xunit;
+using System;
+using Stregsystem.Shared;
+
+namespace StregsystemTests.ProductsTests
+{
+    public class SeasonalProductTests
+    {
+        [Fact]
+        public void IsActive_IsWithinSeasonStartDateAndSeasonEndDateAndIsActive_ReturnsTrue()
+        {
+            // Arrange
+            FakeDateTimeProvider stubDateTimeProvider =
+                new FakeDateTimeProvider(new DateTime(2000, 1, 1, 1, 1, 1));
+
+            
+            SeasonalProduct product = new SeasonalProduct(new FakeId<IProduct>(),
+                                                          new Name(""),
+                                                          new Ddk(0),
+                                                          true,
+                                                          true,
+                                                          new DateTime(2000, 1, 1, 1, 1, 0),
+                                                          new DateTime(2000, 1, 1, 1, 1, 2),
+                                                          stubDateTimeProvider);
+
+            // Act
+            bool isActive = product.IsActive;
+
+            // Assert
+            Assert.True(isActive);
+        }
+
+        [Fact]
+        public void IsActive_IsWithinSeasonStartDateAndSeasonEndDateAndIsNotActive_ReturnsFalse()
+        {
+            // Arrange
+            FakeDateTimeProvider stubDateTimeProvider =
+                new FakeDateTimeProvider(new DateTime(2000, 1, 1, 1, 1, 1));
+
+            
+            SeasonalProduct product = new SeasonalProduct(new FakeId<IProduct>(),
+                                                          new Name(""),
+                                                          new Ddk(0),
+                                                          false,
+                                                          true,
+                                                          new DateTime(2000, 1, 1, 1, 1, 0),
+                                                          new DateTime(2000, 1, 1, 1, 1, 2),
+                                                          stubDateTimeProvider);
+
+            // Act
+            bool isActive = product.IsActive;
+
+            // Assert
+            Assert.False(isActive);
+        }
+
+        [Fact]
+        public void IsActive_IsNotWithinSeasonStartDateAndSeasonEndDateAndIsActive_ReturnsFalse()
+        {
+            // Arrange
+            FakeDateTimeProvider stubDateTimeProvider =
+                new FakeDateTimeProvider(new DateTime(2000, 1, 1, 1, 1, 2));
+
+            
+            
+            SeasonalProduct product = new SeasonalProduct(new FakeId<IProduct>(),
+                                                          new Name(""),
+                                                          new Ddk(0),
+                                                          true,
+                                                          true,
+                                                          new DateTime(2000, 1, 1, 1, 1, 0),
+                                                          new DateTime(2000, 1, 1, 1, 1, 1),
+                                                          stubDateTimeProvider);
+
+            // Act
+            bool isActive = product.IsActive;
+
+            // Assert
+            Assert.False(isActive);
+        }
+    }
+}
