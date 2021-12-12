48228aa6b4a65e206517e713bb98fc904b251d0c
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 18:22:07 +0100

    Update CommandFactoryTests.cs

--- StregsystemTests/CommandsTests/CommandFactoryTests.cs
+++ StregsystemTests/CommandsTests/CommandFactoryTests.cs
@@ -1,20 +1,124 @@
+using System;
+using Stregsystem;
+using Stregsystem.Commands;
+using StregsystemTests.Stubs;
 using Xunit;
 
 namespace StregsystemTests.CommandsTests
 {
     public class CommandFactoryTests
     {
-        [Fact]
-        public void TestName()
+        [Theory]
+        [InlineData(":")]
+        [InlineData(":wrong")]
+        [InlineData("    ")]
+        [InlineData(":q 10")]
+        [InlineData(":quit 10")]
+        [InlineData(":activate 10 10")]
+        [InlineData(":activate")]
+        [InlineData(":deactivate 10 20")]
+        [InlineData(":deactivate")]
+        [InlineData(":crediton 10 20")]
+        [InlineData(":crediton")]
+        [InlineData(":creditoff")]
+        [InlineData(":addcredit casper")]
+        [InlineData(":addcredit 10")]
+        [InlineData(":addcredit casper 10 10")]
+
+        public void InvalidCommandThrowsError(string command)
         {
             // Given
-        
-            // When
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
         
             // Then
+            Assert.Throws<ArgumentException>(() => factory.Parse(command));
         }
 
-        
-    }
+        [Theory]
+        [InlineData(":q")]
+        [InlineData(":quit")]
+        public void ValidQuitCommandReturnsQuitCommand(string commandAsString)
+        {
+            // Given
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
 
+            // When
+            ICommand command = factory.Parse(commandAsString);
+        
+            // Then
+            Assert.IsType<QuitCommand>(command);
+        }
+
+        [Fact]
+        public void ValidActivateCommandReturnsActivateCommand()
+        {
+            // Given
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
+
+            // When
+            ICommand command = factory.Parse(":activate 10");
+        
+            // Then
+            Assert.IsType<ActivateCommand>(command);
+        }
+
+        [Fact]
+        public void ValidDeactivateCommandReturnsDeactivateCommand()
+        {
+            // Given
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
+
+            // When
+            ICommand command = factory.Parse(":deactivate 10");
+        
+            // Then
+            Assert.IsType<DeactivateCommand>(command);
+        }
+
+        [Fact]
+        public void ValidCreditOnCommandReturnsCreditOnCommand()
+        {
+            // Given
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
+
+            // When
+            ICommand command = factory.Parse(":crediton 10");
+        
+            // Then
+            Assert.IsType<CreditOnCommand>(command);
+        }
+
+        [Fact]
+        public void ValidCreditOffCommandReturnsCreditOffCommand()
+        {
+            // Given
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
+
+            // When
+            ICommand command = factory.Parse(":creditoff 10");
+        
+            // Then
+            Assert.IsType<CreditOffCommand>(command);
+        }
+
+        [Fact]
+        public void ValidAddCreditCommandReturnsAddCreditCommand()
+        {
+            // Given
+            CommandFactory factory 
+                = new CommandFactory(new StubStregsystemUI(), new StubStregSystem());
+
+            // When
+            ICommand command = factory.Parse(":addcredit casper 10");
+        
+            // Then
+            Assert.IsType<AddCreditCommand>(command);
+        }
+    }
 }
