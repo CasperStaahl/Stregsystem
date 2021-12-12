c52b031f14690c0d25c9f4690204671c916f7e90
CasperStaahl <cstahl20@student.aau.dk>
2021-12-12 19:55:32 +0100

    Update Program.cs

--- Stregsystem/Program.cs
+++ Stregsystem/Program.cs
@@ -6,9 +6,7 @@ namespace Stregsystem
     {
         private static void Main(string[] args)
         {
-            IStregsystem stregsystem = new Stregsystem(
-                @"../../../products.csv", 
-                @"../../../users.csv");
+            IStregsystem stregsystem = new Stregsystem();
             IStregsystemUI ui = new StregsystemCLI(stregsystem);
             StregsystemController sc = new StregsystemController(ui, stregsystem);
 
