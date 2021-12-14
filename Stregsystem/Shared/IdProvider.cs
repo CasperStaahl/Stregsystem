80c091a5d794b188b6e24c4e97ee54ddc6eb17a8
CasperStaahl <cstahl20@student.aau.dk>
2021-12-14 11:10:19 +0100

    Update IdProvider.cs

--- Stregsystem/Shared/IdProvider.cs
+++ Stregsystem/Shared/IdProvider.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace Stregsystem.Shared
 {
-    class IdProvider<T> : IIdProvider<T>
+    public class IdProvider : IIdProvider
     {
         List<int> _usedIds = new() { 0 };
 
